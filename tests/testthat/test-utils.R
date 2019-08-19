4b7f5d768000c27ccb0e0a6d41e48d7908230bc6
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-08-19 12:30:01 +0200

    added 1x1 test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,4 @@
 test_that("vector to matrix conversion works", {
+  expect_equal(lower_tri_vec_to_mat(c(1)), matrix(c(1),1,1))
   expect_equal(lower_tri_vec_to_mat(c(1,2,1)), matrix(c(1,2,2,1),2,2))
 })
