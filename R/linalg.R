972ac2ecbde913e98cf8f5994a8d5d98d7b28030
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-08-22 10:15:55 +0200

    added lvar_f()

--- R/linalg.R
+++ R/linalg.R
@@ -3,3 +3,6 @@ schur_complement <- function(M, block_index){
   solve(M)[-block_index, -block_index] %>% solve()
   #  M[-block_index, -block_index] - M[block_index, -block_index] %*% MASS::ginv(M[block_index, block_index]) %*% M[-block_index, block_index]
 }
+
+# var(f(x)) with Var(x)=sigma
+lvar_f <- function(dfdx, sigma) t(dfdx) %*% sigma %*% dfdx
--- tests/testthat/test-linear-algebra.R
+++ tests/testthat/test-linear-algebra.R
@@ -2,3 +2,9 @@ test_that("Schur complement calculation works", {
   m <- diag(1:10)
   expect_equal(schur_complement(m, 2), m[-2,-2])
 })
+
+test_that("linearized variance for f is calculated correctly", {
+  df <- c(1,1)
+  omega <- matrix(c(2,1,1,2),2,2)
+  expect_equal(lvar_f(df, omega), matrix(2+2+2*1,1,1))
+})
