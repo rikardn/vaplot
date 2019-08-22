e4c0c20734bb70397176e154cd47128655b4fb4b
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-08-22 10:53:46 +0200

    added function for selecting rows and columns

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,39 @@ lower_tri_vec_to_mat <-  function(vec){
   mat[lower.tri(mat, T)] <- t(mat)[lower.tri(mat, T)]
   return(mat)
 }
+
+# function to drop the named rows and columns in a square matrix
+drc <- function(x, drop){
+  if(NROW(x) == NCOL(x)){
+    vars_to_select <- colnames(x)[!colnames(x) %in% drop]
+    return(x[vars_to_select, vars_to_select, drop = F])
+  }
+  stop("Only square matricies supported")
+}
+# function to drop the named elements in a vector
+dr <- function(x, drop){
+  vars_to_select <- names(x)[!names(x) %in% drop]
+  return(x[vars_to_select, drop = F])
+}
+
+src <- function(x, select){
+  if(NROW(x) == NCOL(x)){
+    vars_to_select <- select[select %in% colnames(x)]
+    return(x[vars_to_select, vars_to_select, drop = F])
+  }
+  stop("Only square matricies supported")
+}
+
+sr <- function(x, select){
+  vars_to_select <- select[select %in% names(x)]
+  return(x[vars_to_select, drop = F])
+}
+
+dsrc <- function(x, drop_rows, select_cols){
+  if(NROW(x) == NCOL(x)){
+    rows_to_select <- colnames(x)[!colnames(x) %in% drop_rows]
+    columns_to_select <- select_cols[select_cols %in% colnames(x)]
+    return(x[rows_to_select, columns_to_select, drop = F])
+  }
+  stop("Only square matricies supported")
+}
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,3 +2,30 @@ test_that("vector to matrix conversion works", {
   expect_equal(lower_tri_vec_to_mat(c(1)), matrix(c(1),1,1))
   expect_equal(lower_tri_vec_to_mat(c(1,2,1)), matrix(c(1,2,2,1),2,2))
 })
+
+test_that("dropping rows and columns in a matrix works", {
+  m <- matrix(1:4, 2, 2, dimnames = list(c("a","b"), c("a", "b")))
+  expect_equal(drc(m, "b"), m[1,1, drop = F])
+})
+
+test_that("selecting rows and columns in a matrix works", {
+  m <- matrix(1:4, 2, 2, dimnames = list(c("a","b"), c("a", "b")))
+  expect_equal(src(m, "b"), m[2,2, drop = F])
+})
+
+test_that("dropping rows and selecting columns in a matrix works", {
+  m <- matrix(1:9, 3, 3, dimnames = list(c("a","b","c"), c("a", "b","c")))
+  expect_equal(dsrc(m, select_cols = "c" , drop_rows = "b"), m[c(1,3), 3, drop = F])
+})
+
+
+
+test_that("dropping rows in a vector works", {
+  vec <- c(a = 1, b = 2, c = 3)
+  expect_equal(dr(vec, "b"), vec[-2])
+})
+
+test_that("selecting rows in a vector works", {
+  vec <- c(a = 1, b = 2, c = 3)
+  expect_equal(sr(vec, "b"), vec[2])
+})
