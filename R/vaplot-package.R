47c2f0bbf278e32b2a3f351692affe2ee883fd48
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-09-03 13:15:59 +0200

    exported dplyr::matches

---
+++ R/vaplot-package.R
@@ -0,0 +1,34 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+
+
+#' matches function
+#'
+#' See \code{dplyr::\link[dplyr:matches]{matches}} for details.
+#'
+#' @name matches
+#' @rdname matches
+#' @keywords internal
+#' @export
+#' @importFrom dplyr matches
+#' @usage read_nm_derivative_results("run.lst", eta_columns = matches("^ETA"))
+NULL
