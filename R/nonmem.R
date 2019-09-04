0a84b6c86f60743679d38f981b50e4fad6f95e4f
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-09-04 09:33:25 +0200

    added additional columns for NM

--- R/nonmem.R
+++ R/nonmem.R
@@ -1,8 +1,12 @@
-nm_column_specs <- function(id = ID, time = TIME, eta = matches("^ETA?\\d+$"), deta = matches("^G\\d+$")){
+nm_column_specs <- function(id = ID, time = TIME, eta = matches("^ETA?\\d+$"),
+                            deta = matches("^G\\d+$"), deps = matches("^H\\d+$"),
+                            deps_deta = matches("^D_EPSETA\\d+_\\d+$")){
   list(
     id = rlang::enquo(id),
     time = rlang::enquo(time),
     eta = rlang::enquo(eta),
-    deta = rlang::enquo(deta)
+    deta = rlang::enquo(deta),
+    deps = rlang::enquo(deps),
+    deps_deta = rlang::enquo(deps_deta)
   )
 }
--- tests/testthat/test-nonmem.R
+++ tests/testthat/test-nonmem.R
@@ -1,5 +1,6 @@
 test_that("column spec returns a list of quosures", {
   specs <- nm_column_specs()
   classes <- purrr::map(specs, class) %>% purrr::map_chr(1)
-  expect_equal(classes, c(id = "quosure", time = "quosure", eta = "quosure", deta = "quosure"))
+  expect_equal(classes, c(id = "quosure", time = "quosure", eta = "quosure",
+                          deta = "quosure", deps = "quosure", deps_deta = "quosure"))
 })
