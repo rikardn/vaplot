c0f36587a96a5712e37b3da5d562dd60562503f5
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-09-10 09:26:46 +0200

    removed idv from column spec

--- R/nonmem.R
+++ R/nonmem.R
@@ -1,9 +1,8 @@
-nm_column_specs <- function(id = ID, time = TIME, eta = matches("^ETA?\\d+$"),
+nm_column_specs <- function(id = ID, eta = matches("^ETA?\\d+$"),
                             deta = matches("^G\\d+$"), deps = matches("^H\\d+$"),
                             deps_deta = matches("^D_EPSETA\\d+_\\d+$")){
   list(
     id = rlang::enquo(id),
-    time = rlang::enquo(time),
     eta = rlang::enquo(eta),
     deta = rlang::enquo(deta),
     deps = rlang::enquo(deps),
