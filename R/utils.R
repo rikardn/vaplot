35b2a6f9abf3fc2a8eaa7da1274b9e50d0209f9b
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-09-17 14:12:33 +0200

    add cnd objects for file reading

--- R/utils.R
+++ R/utils.R
@@ -69,5 +69,14 @@ ui_error <- function(msg, suggestions = NULL){
 }
 
 ui_inform <- function(msg){
-  rlang::inform(msg)
+  rlang::inform(paste("Note:", msg))
 }
+
+cnd_file_not_found <- function(path)
+  rlang::error_cnd("file_not_found", path = path,
+                   message = paste0("File '",path,"' not found."))
+
+cnd_unexpected_file_format <- function(path)
+  rlang::error_cnd("unexpected_file_format", path = path,
+                   message = paste0("The file '",path,"' had an unexpected format."))
+
